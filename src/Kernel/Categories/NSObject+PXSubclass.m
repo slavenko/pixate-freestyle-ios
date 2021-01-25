6005fa0baf3b3f9af4b273a2455785ba02ba3421
Slavenko <slavenko.miljic@nfinnova.com>
2021-01-25 22:54:35 +0100

    - Build fix

--- src/Kernel/Categories/NSObject+PXSubclass.m
+++ src/Kernel/Categories/NSObject+PXSubclass.m
@@ -131,6 +131,7 @@ void PXForceLoadNSObjectPXSubclass() {}
 static BOOL classRespondsToSelectorRAW(Class class, SEL selector)
 {
 	if (@available(iOS 8.0, *)) {
+		NSString *className = NSStringFromClass([class class]);
 		if(
 		   [className containsString:@"pxuiview_uiwebbrowserview"] ||
 		   [className containsString:@"pxuiview_uikeyboardautomatic"] ||
